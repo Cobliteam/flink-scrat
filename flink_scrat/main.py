1491e42e4b5fabec3483ddc979fe9618d3c3125d
Nicolau Tahan <nicolautahan@gmail.com>
2019-06-06 15:41:05 -0300

    Added custom logger and some logs for connector methods

--- flink_scrat/main.py
+++ flink_scrat/main.py
@@ -1,7 +1,38 @@
 import argparse
+import logging
+import os
+import yaml
 
+from envparse import env
+from logging import CRITICAL, ERROR, DEBUG, INFO, WARN
 from flink_scrat.job_manager_connector import FlinkJobmanagerConnector
 
+logger = logging.getLogger(__name__)
+
+def setup_logging():
+    """ Setup logging configuration """
+    default_level = INFO
+    log_level = env('SCRAT_LOG_LEVEL', default=None)
+    if log_level:
+        level_parser = {
+            'CRITICAL': CRITICAL,
+            'ERROR': ERROR,
+            'WARN': WARN,
+            'DEBUG': DEBUG,
+            'INFO': INFO}
+        default_level = level_parser[log_level]
+
+    path = 'logging.yaml'
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+            if log_level:
+                loggers = config.setdefault('loggers', {})
+                delay_ruler_logger = loggers.setdefault('delay_ruler', {})
+                delay_ruler_logger['level'] = default_level
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
 
 def parse_args():
 	parser = argparse.ArgumentParser(description="A python client to deploy Flink applications to a remote cluster")
@@ -19,6 +50,9 @@ def parse_args():
 	submit_parser.add_argument("--jar-path", dest="jar_path", required=True,
 		help="Path for jar to be deployed")
 
+	submit_parser.add_argument("--target-dir", dest="target_dir", required=False,
+		help="Target directory to log job savepoints")
+
 	submit_parser.add_argument("--job-id", dest="job_id", required=False,
 		help="Unique identifier for job to be restored")
 
@@ -28,6 +62,7 @@ def parse_args():
 	return args
 
 def main():
+	setup_logging()
 	args = parse_args()
 
 	address = args.address
@@ -37,7 +72,7 @@ def main():
 	conn = FlinkJobmanagerConnector(address, port)
 
 	if action == "submit":
-		conn.submit_job(args.jar_path, args.job_id)
+		conn.submit_job(args.jar_path, args.target_dir, args.job_id)
 
 if __name__ == "__main__":
 	main()
