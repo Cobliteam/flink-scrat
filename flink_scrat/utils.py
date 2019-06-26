7b11636826f58a438564b02be7d9c8c20dce1fb5
vinyxr4 <vinicius@cobli.co>
2019-06-26 19:49:14 -0300

    Querying running apps using query params

--- flink_scrat/utils.py
+++ flink_scrat/utils.py
@@ -39,34 +39,34 @@ def handle_response(req_response):
 
 
 def find_manager_address(yarn_address, yarn_port, app_name):
-    apps_info = _get_apps_info(yarn_address, yarn_port)
-
-    running_app = _get_running_app(apps_info, app_name)
+    running_app = _get_running_app(yarn_address, yarn_port, app_name)
 
     rpc_addres_info = _build_rpc_address_info(running_app)
 
     return rpc_addres_info
 
 
-def _get_apps_info(yarn_address, yarn_port):
-    route = "http://{}:{}/ws/v1/cluster/apps".format(yarn_address, yarn_port)
+def _get_running_app(yarn_address, yarn_port, app_name):
+    route = 'http://{}:{}/ws/v1/cluster/apps'.format(yarn_address, yarn_port)
 
-    result = handle_response(requests.get(route))
+    params = {
+        'states': ["RUNNING"],
+        'applicationTags': ["{}".format(app_name)],
+        'limit': 2
+    }
 
-    apps_info = result['apps']['app']
+    result = handle_response(requests.get(route, params=params))
+    running_apps_info = result['apps']['app']
 
-    return apps_info
+    num_running_apps = len(running_apps_info)
 
+    if num_running_apps != 1:
+        if num_running_apps == 0:
+            raise Exception("No app found with state=<RUNNING> and tag=<{}>".format(app_name))
+        else:
+            raise Exception("More then one app found with state=<RUNNING> and tag=<{}>".format(app_name))
 
-def _get_running_app(apps_info, app_name):
-    running_apps = list(filter(lambda x: x['state'] == 'RUNNING' and x['name'] == app_name, apps_info))
-
-    if len(running_apps) == 0:
-        raise Exception("No app found with state=<RUNNING> and name=<{}>".format(app_name))
-    elif len(running_apps) > 1:
-        raise Exception("More then one app found with state=<RUNNING> and name=<{}>".format(app_name))
-
-    running_app = running_apps[0]
+    running_app = running_apps_info[0]
 
     return running_app
 
